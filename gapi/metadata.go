aeb2677cca837ab72cdfd7e5d9130d30437f0a9e
Haniif Ahmad C <haniifcandra@gmail.com>
2025-07-19 04:06:37 +0800

    fix: store ip address in session from grpc request

--- gapi/metadata.go
+++ gapi/metadata.go
@@ -2,8 +2,10 @@ package gapi
 
 import (
 	"context"
+	"log"
 
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
 )
 
 const (
@@ -21,7 +23,7 @@ func (server *Server) extractMetadata(ctx context.Context) *Metadata {
 	md := &Metadata{}
 
 	if m, ok := metadata.FromIncomingContext(ctx); ok {
-		// log.Printf("Metadata context: %+v\n", m)
+		log.Printf("Metadata context: %+v\n", m)
 
 		if userAgent := m.Get(userAgentHeader); len(userAgent) > 0 {
 			md.UserAgent = userAgent[0]
@@ -34,6 +36,10 @@ func (server *Server) extractMetadata(ctx context.Context) *Metadata {
 		if ipAddress := m.Get(ipAddressHeader); len(ipAddress) > 0 {
 			md.IpAddress = ipAddress[0]
 		}
+	}
+
+	if p, ok := peer.FromContext(ctx); ok {
+		md.IpAddress = p.Addr.String()
 	}
 
 	return md
